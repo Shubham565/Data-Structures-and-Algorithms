7cc060cf079326ba1c58aec95d1a8d74a7bb84cc
Shubham Bhambey <shubham.bhambey8@gmail.com>
2025-03-14 23:09:34 +0530

    more cleanup

--- Educative/src/com/bhambey/multithreading/RaceConditionDemo.java
+++
@@ -1,9 +0,0 @@
-package com.bhambey.multithreading;
-
-public class RaceConditionDemo {
-
-	public static void main(String[] args) throws InterruptedException {
-		RaceCondition.runTest();
-	}
-
-}
--- Educative/src/com/bhambey/twopointers/ValidPalindrome.java
+++
@@ -1,9 +0,0 @@
-package com.bhambey.twopointers;
-
-public class ValidPalindrome {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
